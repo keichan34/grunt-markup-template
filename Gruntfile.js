8f763daa3bcdcc2b1c05b76924e472b391f32a67
Keitaroh Kobayashi <keita@kkob.us>
2013-11-28 11:38:22 +0900

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,108 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Compile steps
+    // = CoffeeScript => JavaScript
+    coffee: {
+      glob_to_multiple: {
+        expand: true,
+        flatten: true,
+        cwd: 'js/src',
+        src: ['*.js.coffee'],
+        dest: 'js/src',
+        ext: '.js'
+      }
+    },
+    // SCSS => CSS
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'css/src',
+            src: ['*.scss'],
+            dest: 'css/src',
+            ext: '.css'
+          }
+        ]
+      }
+    },
+    // Slim => HTML
+    slim: {
+      dist: {
+        options: {
+          pretty: true
+        },
+        files: [
+          {
+            expand: true,
+            cwd: '.',
+            src: ['*.slim'],
+            dest: '.',
+            ext: '.html'
+          }
+        ]
+      }
+    },
+    // Compression steps
+    // = JavaScript minify
+    uglify: {
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'js/src',
+            src: ['*.js'],
+            dest: 'js',
+            ext: '.min.js'
+          }
+        ]
+      }
+    },
+    // = CSS Minify
+    cssmin: {
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'css/src',
+            src: ['*.css'],
+            dest: 'css',
+            ext: '.min.css'
+          }
+        ]
+      }
+    },
+    // Watch for file changes
+    watch: {
+      scripts: {
+        files: ['js/src/*.js.coffee'],
+        tasks: ['newer:coffee', 'newer:uglify']
+      },
+      styles: {
+        files: ['css/src/*.scss'],
+        tasks: ['newer:sass', 'newer:cssmin']
+      },
+      templates: {
+        files: ['*.slim'],
+        tasks: ['newer:slim']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-newer');
+  grunt.loadNpmTasks('grunt-slim');
+
+  grunt.registerTask('default', ['watch']);
+
+};
---
+++ README.md
@@ -0,0 +1,10 @@
+# grunt-markup-template
+
+A quick template to get you started marking up your website.
+
+## Usage
+
+```
+$ npm install
+$ grunt watch
+```
